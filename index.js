8695979b0d4b458f4d0ab21e2346b5e7ec60ead1
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-10-26 15:14:09 +0600

    create course details API

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 const port = process.env.PORT || 8000;
+app.use(express.static("public"));
+
+const courses = require("./Data/Courses.json");
+const courseDetails = require("./Data/CourseDetails.json");
 
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const courseData = courses.find((course) => course.id == id);
+  res.send(courseData);
+});
+
+app.get("/course/:cat_id", (req, res) => {
+  const cat_id = req.params.cat_id;
+  const details = courseDetails.filter((course) => course.cat_id == cat_id);
+  res.send(details);
 });
 
 app.listen(port, () => {
