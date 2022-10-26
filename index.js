906ba8aea9384bb374184f38dbbbb34635b6dc6b
MOTIUR <motiur@MOTIURs-MacBook-Pro.local>
2022-10-26 20:31:46 +0600

    add api for specific course content

--- index.js
+++ index.js
@@ -14,19 +14,32 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Get all the course API
 app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
+//Get specific course data based on id API
 app.get("/courses/:id", (req, res) => {
   const id = req.params.id;
   const courseData = courses.find((course) => course.id == id);
   res.send(courseData);
 });
 
+// Get all the specific category content API
 app.get("/course/:cat_id", (req, res) => {
   const cat_id = req.params.cat_id;
   const details = courseDetails.filter((course) => course.cat_id == cat_id);
+  res.send(details);
+});
+
+// Get specific category and id content API
+app.get("/course/:cat_id/:id", (req, res) => {
+  const cat_id = req.params.cat_id;
+  const id = req.params.id;
+  const details = courseDetails.find(
+    (course) => course.id == id && course.cat_id == cat_id
+  );
   res.send(details);
 });
 
